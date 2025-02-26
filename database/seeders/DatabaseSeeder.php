05b0eef59e9ea507d4aa2f7e2258a076e78c1106
kaunghtunthant <kaung@xanits.com>
2025-02-27 00:56:13 +0400

    User seeder added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
             CategorySeeder::class,
             ItemSeeder::class,
             EventSeeder::class,
+            DefaultUserSeeder::class,
         ]);
     }
 }
